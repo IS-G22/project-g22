2653514f534796842b9c0493020fe84ca00ab3a3
Pigna <andrea_pigna99@hotmail.it>
2021-11-29 15:24:10 +0100

    formattazione data

--- ui/prenotazioni.js
+++ ui/prenotazioni.js
@@ -7,7 +7,7 @@ const prenotazioni = {
                 <img src="./photo/lavatrice.jpg" class="img-lavatrice"></img>
                 <div class="box-info">
                     <h4>Lavatrice {{pren.id_lavatrice}}</h4>
-                    <div>{{(new Date(pren.data))}}</div>
+                    <div>{{pren.formatted_data}}</div>
                 </div>
             </div>
         
@@ -25,6 +25,9 @@ const prenotazioni = {
             .then((response)=>{
                 this.prenotazioni=response.data;
                 console.log(this.prenotazioni);
+                this.prenotazioni.forEach((el, index, arr)=>{
+                    arr[index].formatted_data = formattaData(new Date(el.data));
+                })
             });
         },
     },
--- ui/variables.js
+++ ui/variables.js
@@ -1,3 +1,18 @@
 const variables ={
     API_URL:"http://localhost:49146/api/",
 }
+
+function formattaData(data){
+    let giorno;
+    switch(data.getDay()){
+        case 0:giorno="Lunedì"; break;
+        case 1:giorno="Martedì"; break;
+        case 2:giorno="Mercoledì"; break;
+        case 3:giorno="Giovedì"; break;
+        case 4:giorno="Venerdì"; break;
+        case 5:giorno="Sabato"; break;
+        case 6:giorno="Domenica"; break;
+        default:giorno="Boh";
+    }
+    return giorno;
+}
