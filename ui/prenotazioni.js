25f2ef3469540f1d951d6eb7d5119a1dc8ab7cf2
Pigna <andrea_pigna99@hotmail.it>
2021-11-29 14:08:57 +0100

    lista prenotazione ui

--- ui/prenotazioni.js
+++ ui/prenotazioni.js
@@ -1,14 +1,18 @@
 const prenotazioni = {
-    template: `<div>
+    template: `<div class="main">
     <h2>Lista Prenotazioni</h2>
-    
-    
-    
-    
-    
-    
-    
-    
+    <div class="list">
+        <section class="prenotazione" v-for="pren in prenotazioni">
+            <div class="box">
+                <img src="./photo/lavatrice.jpg" class="img-lavatrice"></img>
+                <div class="box-info">
+                    <h4>Lavatrice {{pren.id_lavatrice}}</h4>
+                    <div>{{(new Date(pren.data))}}</div>
+                </div>
+            </div>
+        
+        </section>
+    </div>
     </div>`,
     data(){
         return{
