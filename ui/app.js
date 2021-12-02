83aea8edfabdd04bd76ddf093300c097f9fb7576
Pigna <andrea_pigna99@hotmail.it>
2021-12-02 12:15:30 +0100

    language!

--- ui/app.js
+++ ui/app.js
@@ -8,7 +8,18 @@ const router=new VueRouter({
     routes
 })
 
+// Ready translated locale messages
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+    locale: 'it', // set locale
+    messages: languages, // set locale messages
+})
+
+
+
 const app = new Vue({
+    i18n,
     router,
     data: {
         isActive: window.location.hash,
@@ -29,6 +40,13 @@ const app = new Vue({
             this.isToggle= !this.isToggle;
         },
         toggleLanguage: function(){
+            //manca localstorage
+            //console.log(i18n.locale)
+            if(i18n.locale=='it'){
+                i18n.locale='en';
+            }else{
+                i18n.locale='it';
+            }
             this.isEng= !this.isEng;
         }
     },
---
+++ ui/languages.js
@@ -0,0 +1,18 @@
+const languages =
+{
+    en: {
+      menu: {
+        prenotazioni: "Reservations",
+        guasto: "Reports failure",
+        nuovaprenotazione: "New Reservation"
+
+      }
+    },
+    it: {
+      menu: {
+        prenotazioni: "Prenotazioni",
+        guasto: "Segnala guasto",
+        nuovaprenotazione: "Nuova Prenotazione"
+      }
+    }
+}
